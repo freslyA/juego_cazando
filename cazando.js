971ec9344c9a2faff1ecd85333553d9895076d6b
freslyA <freslyandino@gmail.com>
2026-04-12 19:39:03 -0500

    variables y constantes en conjuntos con las funciones iniciarJuego y dibujar comida funciona correctamente

--- cazando.js
+++ cazando.js
@@ -1,7 +1,22 @@
 let canvas= document.getElementById("areaJuego")
 let canva=canvas.getContext("2d")
-
+let gatoX=canvas.width/2
+let gatoY=canvas.height/2
+let comidaX=canvas.width
+let comidaY=canvas.height
+const ALTO_GATO=20
+const ANCHO_GATO=40
+const ALTO_COMIDA=20
+const ANCHO_COMIDA=20
 function dibujarGato(){
     canva.fillStyle="red"
-    canva.fillRect(canvas.width/2,canvas.height/2,20,20)
+    canva.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO)
+}
+function dibujarComida(){
+    canva.fillStyle="brown"
+    canva.fillRect(comidaX-ANCHO_COMIDA,comidaY-ALTO_COMIDA,ANCHO_COMIDA,ALTO_COMIDA)
+}
+function inciarJuego(){
+    dibujarComida()
+    dibujarGato()
 }
